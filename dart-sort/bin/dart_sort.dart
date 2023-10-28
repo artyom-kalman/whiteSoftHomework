3568c38f548d0ac1616a3a6493515cb0593d1928
ArtyomMB <kalashnikov252500@gmail.com>
2023-10-28 21:02:06 +1000

    init commit

---
+++ dart-sort/bin/dart_sort.dart
@@ -0,0 +1,5 @@
+import 'package:dart_sort/dart_sort.dart' as dart_sort;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_sort.calculate()}!');
+}
---
+++ dart-sort/test/dart_sort_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_sort/dart_sort.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
