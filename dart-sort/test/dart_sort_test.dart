ade60d40c86f2023b1e840415c5ba380570b1032
ArtyomMB <kalashnikov252500@gmail.com>
2023-10-29 11:38:56 +1000

    add tests for quickSort function

--- dart-sort/test/dart_sort_test.dart
+++ dart-sort/test/dart_sort_test.dart
@@ -2,7 +2,30 @@ import 'package:dart_sort/dart_sort.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
+    group('quickSort tests', () {
+        test('test #1', () {
+            final actual = quickSort([3, 2, 5, 0, 1, 8, 7, 6, 9, 4]);
+            final expected = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+            expect(actual, expected);
+        });
+
+        test('test #2', () {
+            var array = <num>[0, 1, 0, 1, -1, -2, -3, 0, 1, 0, 1];
+            final actual = quickSort(array);
+            array.sort();
+
+            expect(actual, array);
+        });
+
+        test('test #3', () {
+            var array = <num>[-3.2, -500.5, 1003.67, 43.09, 6];
+            final actual = quickSort(array);
+            array.sort();
+
+            expect(actual, array);
+        });
+
+    });
+
 }
